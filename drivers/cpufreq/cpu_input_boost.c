530b1e81da5f7b93ac3e7063fbd7d0bb564669da
kdrag0n <dragon@khronodragon.com>
2018-12-09 11:35:50 -0800

    msm: mdss: Boost only if last interaction was within 1s
    This should prevent AOD notification boosting and excessive boosting
    when watching videos.
    
    Signed-off-by: kdrag0n <dragon@khronodragon.com>
    Signed-off-by: Subhajeet Muhuri <kenny3fcb@gmail.com>

--- drivers/cpufreq/cpu_input_boost.c
+++ drivers/cpufreq/cpu_input_boost.c
@@ -19,6 +19,8 @@
 #include <uapi/linux/sched/types.h>
 #endif
 
+unsigned long last_input_time;
+
 enum {
 	SCREEN_OFF,
 	INPUT_BOOST,
@@ -248,6 +250,8 @@ static void cpu_input_boost_input_event(struct input_handle *handle,
 	struct boost_drv *b = handle->handler->private;
 
 	__cpu_input_boost_kick(b);
+
+	last_input_time = jiffies;
 }
 
 static int cpu_input_boost_input_connect(struct input_handler *handler,
