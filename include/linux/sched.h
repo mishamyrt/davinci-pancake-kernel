5f081386ca8dffdec8d9a1b0c50cb35c78488dda
Danny Lin <danny@kdrag0n.dev>
2019-03-16 21:47:36 -0700

    kthread: Add API to create kthreads bound to low-power CPUs
    Some unimportant kthreads do not need high-performance and can safely be
    bound to low-power CPUs for energy efficiency.
    
    This commit adds an API to create kthreads bound to low-power CPUs. This
    affinity cannot be altered by the kernel or userspace.
    
    This extends the performance-critical kthread support introduced in
    commit c4a0ef7e1d1e ("kernel: Add API to mark IRQs and kthreads as
    performance critical").
    
    Signed-off-by: Danny Lin <danny@kdrag0n.dev>

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -1306,7 +1306,6 @@ struct task_struct {
 	/* Bitmask and counter of trace recursion: */
 	unsigned long			trace_recursion;
 #endif /* CONFIG_TRACING */
-
 #ifdef CONFIG_KCOV
 	/* Coverage collection mode enabled for this task (0 if disabled): */
 	enum kcov_mode			kcov_mode;
@@ -1577,6 +1576,7 @@ extern struct pid *cad_pid;
 #define PF_SWAPWRITE		0x00800000	/* Allowed to write to swap */
 #define PF_MEMSTALL		0x01000000	/* Stalled due to lack of memory */
 #define PF_PERF_CRITICAL	0x02000000	/* Thread is performance-critical */
+#define PF_LOW_POWER		0x10000000	/* Thread is low-power */
 #define PF_NO_SETAFFINITY	0x04000000	/* Userland is not allowed to meddle with cpus_allowed */
 #define PF_MCE_EARLY		0x08000000      /* Early kill for mce process policy */
 #define PF_WAKE_UP_IDLE         0x10000000	/* TTWU on an idle CPU */
