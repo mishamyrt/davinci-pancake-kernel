85cc2143574cf6011d75a482750bdaa6b816503a
Qian Cai <cai@lca.pw>
2019-03-28 20:43:15 -0700

    UPSTREAM: kasan: fix variable 'tag' set but not used warning
    Upstream commit c412a769d2452161e97f163c4c4f31efc6626f06.
    
    set_tag() compiles away when CONFIG_KASAN_SW_TAGS=n, so make
    arch_kasan_set_tag() a static inline function to fix warnings below.
    
      mm/kasan/common.c: In function '__kasan_kmalloc':
      mm/kasan/common.c:475:5: warning: variable 'tag' set but not used [-Wunused-but-set-variable]
        u8 tag;
           ^~~
    
    Link: http://lkml.kernel.org/r/20190307185244.54648-1-cai@lca.pw
    Signed-off-by: Qian Cai <cai@lca.pw>
    Reviewed-by: Andrey Konovalov <andreyknvl@google.com>
    Cc: Andrey Ryabinin <aryabinin@virtuozzo.com>
    Cc: Alexander Potapenko <glider@google.com>
    Cc: Dmitry Vyukov <dvyukov@google.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    Signed-off-by: Andrey Konovalov <andreyknvl@google.com>
    Change-Id: I04b94f7e65044e101edf6c1bbc9bd7014783864c
    Bug: 128674696

--- arch/arm64/kernel/traps.c
+++ arch/arm64/kernel/traps.c
@@ -975,9 +975,9 @@ static struct break_hook refcount_break_hook = {
 /* This registration must happen early, before debug_traps_init(). */
 void __init trap_init(void)
 {
-	register_break_hook(&bug_break_hook);
+	register_kernel_break_hook(&bug_break_hook);
 	register_kernel_break_hook(&refcount_break_hook);
 #ifdef CONFIG_KASAN_SW_TAGS
-	register_break_hook(&kasan_break_hook);
+	register_kernel_break_hook(&kasan_break_hook);
 #endif
 }
