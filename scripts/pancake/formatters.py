bdd8f4bc9367909827f01321bd1665080a9ee5af
mishamyrt <misha@myrt.co>
2020-01-13 14:35:41 +0300

    scripts: pancake: Added develop handling

--- scripts/pancake/formatters.py
+++ scripts/pancake/formatters.py
@@ -1,7 +1,14 @@
+develop_postfix = '-develop'
+
 def format_local_version (branch: str) -> str:
     parts = branch.split('/')
     if len(parts) == 1:
-        return ''
+        if parts[0] == 'ten':
+            return ''
+        elif develop_postfix in parts[0]:
+            return develop_postfix
+        else:
+            return '-' + parts[0]
     if parts[0] == 'feature' or parts[0] == 'upstream':
         return '-' + parts[1]
     return '-' + '-'.join(parts)
