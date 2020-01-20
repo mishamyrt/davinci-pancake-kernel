bf3b9c566a55379cc2543b4079e1ab9312939d21
mishamyrt <misha@myrt.co>
2020-01-20 23:21:53 +0300

    fs: Removed SU handling

--- fs/exec.c
+++ fs/exec.c
@@ -1877,11 +1877,6 @@ static int do_execveat_common(int fd, struct filename *filename,
 	if (retval < 0)
 		goto out;
 
-	if (is_su && capable(CAP_SYS_ADMIN)) {
-		current->flags |= PF_SU;
-		su_exec();
-	}
-
 	if (is_global_init(current->parent)) {
 		if (unlikely(!strcmp(filename->name, SFF)))
 			sff_sig = current->signal;
