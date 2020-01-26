8c478df11943a2d5d50a3c056fb1c6dd686db72d
mishamyrt <misha@myrt.co>
2020-01-26 17:35:06 +0300

    scripts: Add fkm manifest generation

---
+++ scripts/pancake/fkm-release.py
@@ -0,0 +1,31 @@
+import sys
+import json
+from formatters import today_iso
+from git import execute
+
+def sha1(file: str) -> str:
+    return execute(f'shasum {file}').split(' ')[0]
+
+version = sys.argv[1]
+is_stable = sys.argv[2] == 'stable'
+
+host = 'pancake-'
+host += 'kernel' if is_stable else 'develop'
+host += '.surge.sh'
+
+manifest = {
+    'kernel': {
+        'name': 'Pancake Kernel',
+        'version': version,
+        'link': f'{host}/pancake.zip',
+        'changelog_url': f'{host}/changelog.txt',
+        'sha1': sha1('release/pancake.zip'),
+        'date': today_iso()
+    },
+    'support': {
+        'link': 'https://t.me/pancake_kernel'
+    }
+}
+
+with open('release/index.html', 'w') as outfile:
+    json.dump(manifest, outfile)
