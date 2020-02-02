zachariasmaladroit/kernel_lge_msm8998

mm: Implement minimum bound for performing readahead
For requests that are less than VM_MIN_READAHEAD, we should skip
readahead if it is requested. Small readaheads will increase latency for
small lookups.

Signed-off-by: Tyler Nijmeh <tylernij@gmail.com>
(cherry picked from commit b9a9ac0be338bdae9a3ceb07ffc1c69436c7882d)
Signed-off-by: zachariasmaladroit <zacharias.maladroit@gmail.com>

mm/readahead.c
@@ -161,6 +161,13 @@ int __do_page_cache_readahead(struct address_space *mapping, struct file *filp,
 	int ret = 0;
 	loff_t isize = i_size_read(inode);
 
+	/*
+	 * Do not perform a readahead if the requested size is less
+	 * than the minimum readahead value specified.
+	 */
+	if (nr_to_read < (VM_MIN_READAHEAD * 1024) / PAGE_CACHE_SIZE)
+		goto out;
+
 	if (isize == 0)
 		goto out;
 

mishamyrt/davinci-pancake-kernel

mm/readahead.c
@@ -160,6 +160,13 @@ int __do_page_cache_readahead(struct address_space *mapping, struct file *filp,
 	loff_t isize = i_size_read(inode);
 	gfp_t gfp_mask = readahead_gfp_mask(mapping);
 
+	/*
+	 * Do not perform a readahead if the requested size is less
+	 * than the minimum readahead value specified.
+	 */
+	if (nr_to_read < (VM_MIN_READAHEAD * 1024) / PAGE_SIZE)
+		goto out;
+
 	if (isize == 0)
 		goto out;
 