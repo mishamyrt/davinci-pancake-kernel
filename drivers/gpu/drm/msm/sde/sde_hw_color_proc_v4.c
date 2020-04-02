026b0609d9c2cc715b1d4d3edaed2698d8233198
mishamyrt <misha@myrt.co>
2020-04-02 19:59:10 +0000

    kcal: Tune davinci screen colour calibration

--- drivers/gpu/drm/msm/sde/sde_hw_color_proc_v4.c
+++ drivers/gpu/drm/msm/sde/sde_hw_color_proc_v4.c
@@ -15,11 +15,11 @@
 #include "sde_hw_color_proc_common_v4.h"
 #include "sde_hw_color_proc_v4.h"
 
-unsigned int kcal_red = 256;
-unsigned int kcal_green = 256;
-unsigned int kcal_blue = 256;
+unsigned int kcal_red = 255;
+unsigned int kcal_green = 242;
+unsigned int kcal_blue = 241;
 unsigned int kcal_hue = 0;
-unsigned int kcal_sat = 255;
+unsigned int kcal_sat = 256;
 unsigned int kcal_val = 255;
 unsigned int kcal_cont = 255;
 
